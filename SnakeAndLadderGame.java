c95b317697c90e2551f9a1bbee85e52a1da0209d
neil245 <indranilghoshal5@gmail.com>
2021-05-15 18:40:45 +0530

    New file added

---
+++ SnakeAndLadderGame.java
@@ -0,0 +1,9 @@
+package SnakeAndLadderGame;
+public class SnakeAndLadderGame {
+	public static void main(String[] args) {
+		System.out.println("Snakes & Ladder Game");
+		int init_position = 0;
+		System.out.println(init_position);
+	}
+
+}
